97b592f5518a8cce2c5e8376aad0f4961e4423a9
sreeramavipparti <sreerama_vipparti@yahoo.co.in>
2017-12-09 12:16:18 +0530

    Refactoring

--- Sources/Check.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(int argc, char *argv[]) {
-  int i=0;
-  printf("Output from %s\n",argv[0]);
-  for(i=1;i<argc;i++) {
-    printf(argv[i]);
-    printf("\n");
-  }
-} /* void main(int argc, char *argv[]) */
